c7ee1864a967d38d1d15b1571cc1f7ab45a177a7
snb1708 <snb.mymail@gmail.com>
2015-10-25 20:46:49 +0100

    This is a file with 2 functions from the exercise.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two training functions which help to understand
+## the scoping rules in R
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) ## this function creates list of functions
+{
+  
+  myInverseMatrix<-NULL ## initialize and set matrix in NULL
+  
+  setMatrix<-function(y=matrix()) ## function which set matrix 
+  {
+    x<<-y  ## set x as a matrix which is equal to the input matrix y
+    myInverseMatrix<<-NULL  ## initialize and set inverse matrix in NULL 
+  } 
+  
+  getMatrix<-function() x ## return the matrix which is stored in x
+  
+  setInverseMatrix<-function(InverseMatrix) ## function which set inverse matrix 
+  {
+    myInverseMatrix<<-InverseMatrix ##set myInverseMatrix as a matrix which is equal to the input matrix 
+  }
+  
+  getInverseMatrix<-function() myInverseMatrix ## return the inverse matrix which is stored in myInverseMatrix
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  k<-x$getInverseMatrix() 
+  if (!is.null(k)) ## check if inverse matrix exist in cache
+  {
+    message("getting cached Inverse Matrix") ## return cached matrix
+    return(k)
+  }
+  else ## calculates and stores inverse matrix
+  {
+    message("getting calculated Inverse Matrix")
+    myMatrix<-x$getMatrix()
+    myInverseMatrix<-solve(myMatrix)
+    x$setInverseMatrix(myInverseMatrix)
+    myInverseMatrix
+  }
 }
+
